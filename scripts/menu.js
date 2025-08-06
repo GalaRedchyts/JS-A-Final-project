107f7c6a942f3a4a1203bd83ba0d71a48ba63117
Halyna.Redchyts <galynkared@gmail.com>
2025-08-06 15:14:36 +0300

    add menu

---
+++ scripts/menu.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/components/menu.html')
+    .then(res => res.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const nav = parser.parseFromString(data, 'text/html').querySelector('nav');
+      document.querySelector('[data-header]').appendChild(nav);
+    });
+});
